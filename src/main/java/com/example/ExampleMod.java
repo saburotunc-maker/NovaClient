76511e307bc961f0c8328daf41cd36d048c612ee
saburotunc-maker <saburotunc@gmail.com>
2026-08-11 11:50:31 +0300

    Refactor ExampleMod to implement ClientModInitializer

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,11 +1,31 @@
 package com.example;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
 
-public class ExampleMod implements ModInitializer {
+public class ExampleMod implements ClientModInitializer {
+    private static KeyBinding openKey;
+
     @Override
-    public void onInitialize() {
-        ClientInput.register();
+    public void onInitializeClient() {
+        openKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.novaclient.open",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_Y,
+                "category.novaclient"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (openKey.wasPressed()) {
+                if (client.player != null) {
+                    client.player.sendMessage(Text.literal("§a[NovaClient] Mod Aktif!"), false);
+                }
+            }
+        });
     }
 }
-
